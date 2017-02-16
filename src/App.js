c006d862d182235ec5ef48981e04123953ef638d
Robert Rees <shudderfix@gmail.com>
2017-02-16 19:09:49 +0000

    Creates some data on the Game instance to hold the game state

--- src/App.js
+++ src/App.js
@@ -11,8 +11,34 @@ class App extends Component {
         <p className="App-intro">
           Guess the number I&rsquo;m thinking of.
         </p>
+        <Game />
       </div>
     );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game">
+        <p>{this.state.targetNumber} {this.maxGuessableNumber}</p>
+        <button className="Game--GuessButton">1</button>
+      </div>
+    )
+  }
+
+  constructor(props) {
+    super(props);
+
+    const maxGuessableNumber = 10;
+
+    this.maxGuessableNumber = maxGuessableNumber;
+
+    const target = Math.floor(Math.random() * maxGuessableNumber);
+    this.state = {
+      targetNumber: target + 1,
+      playing: true
+    }
   }
 }
 
